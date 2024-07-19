f4bbdeba8357a47eab5d08832059037bbdd0471c
aamir1234556789 <101567656+aamir1234556789@users.noreply.github.com>
2024-07-19 15:06:19 +0500

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Learning OpenShift is too much fun!!\n";
-print "I am learning open shift from Tech Tajendra youtube channel!!\n";
+print "I am learning open shift from Tech Tajendra youtube channel!+!\n";
 ?>
